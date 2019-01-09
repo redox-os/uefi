e5b31b953791ea492c270bbe1ba07564ceea3944
Marcel Hellwig <git@cookiesoft.de>
2019-01-09 15:37:00 +0100

    Changed `*mut ...` to `Option<NonNull<...>>`
    Instead of a raw pointer use a combination of Option and NonNull which
    is exactly the same size as a raw pointer, expect you cannot access a
    nullpointer by accident.
    The assembly is completly identical on the hot path (the one that does not
    procude a panic). Only one move more on the cold path (because of the
    unwrap message (?).

--- uefi_alloc/src/lib.rs
+++ uefi_alloc/src/lib.rs
@@ -7,47 +7,38 @@ extern crate uefi;
 
 use core::alloc::{GlobalAlloc, Layout};
 use core::ops::Try;
-use core::ptr;
+use core::ptr::{self, NonNull};
 use uefi::memory::MemoryType;
 use uefi::system::SystemTable;
 
-static mut UEFI: *mut SystemTable = 0 as *mut SystemTable;
+static mut UEFI: Option<NonNull<SystemTable>> = None;
 
 pub unsafe fn init(table: &'static mut SystemTable) {
-    UEFI = table;
-}
-
-fn get_uefi() -> Option<&'static mut SystemTable> {
-    unsafe {
-        if UEFI as usize == 0 {
-            None
-        } else {
-            Some(&mut *UEFI)
-        }
-    }
+    UEFI = NonNull::new(table);
 }
 
 pub struct Allocator;
 
 unsafe impl GlobalAlloc for Allocator {
     unsafe fn alloc(&self, layout: Layout) -> *mut u8 {
-        if let Some(ref mut uefi) = get_uefi() {
-            let mut ptr = 0;
-            if let Err(_) = (uefi.BootServices.AllocatePool)(MemoryType::EfiLoaderData, layout.size(), &mut ptr).into_result() {
-                ptr::null_mut()
-            } else {
-                ptr as *mut u8
-            }
+        let uefi = UEFI.expect("__rust_allocate: uefi not initialized");
+        let mut ptr = 0;
+        if (uefi.as_ref().BootServices.AllocatePool)(
+            MemoryType::EfiLoaderData,
+            layout.size(),
+            &mut ptr,
+        )
+        .into_result()
+        .is_ok()
+        {
+            ptr as *mut u8
         } else {
-            panic!("__rust_allocate: uefi not initialized");
+            ptr::null_mut()
         }
     }
 
     unsafe fn dealloc(&self, ptr: *mut u8, _layout: Layout) {
-        if let Some(ref mut uefi) = get_uefi() {
-            let _ = (uefi.BootServices.FreePool)(ptr as usize);
-        } else {
-            panic!("__rust_deallocate: uefi not initialized");
-        }
+        let uefi = UEFI.expect("__rust_deallocate: uefi not initialized");
+        let _ = (uefi.as_ref().BootServices.FreePool)(ptr as usize);
     }
 }
