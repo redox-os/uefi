redox-os/uefi

commit 619d351b6bec9005b9b7c4f0e705680b310f3963

Update uefi dep, fix building on nightly
The UEFI crate has been updated to use try_trait_v2, which changes how
Status is used.

Signed-off-by: Tim Crawford <tcrawford@system76.com>

uefi_alloc/src/lib.rs
@@ -1,10 +1,10 @@
 #![feature(allocator_api)]
-#![feature(const_fn)]
-#![feature(try_trait)]
+#![feature(try_trait_v2)]
+#![feature(control_flow_enum)]
 #![no_std]
 
 use core::alloc::{GlobalAlloc, Layout};
-use core::ops::Try;
+use core::ops::{ControlFlow, Try};
 use core::ptr::{self, NonNull};
 use uefi::memory::MemoryType;
 use uefi::system::SystemTable;
@@ -21,17 +21,16 @@ unsafe impl GlobalAlloc for Allocator {
     unsafe fn alloc(&self, layout: Layout) -> *mut u8 {
         let uefi = UEFI.expect("__rust_allocate: uefi not initialized");
         let mut ptr = 0;
-        if (uefi.as_ref().BootServices.AllocatePool)(
+        let res = (uefi.as_ref().BootServices.AllocatePool)(
             MemoryType::EfiLoaderData,
             layout.size(),
             &mut ptr,
         )
-        .into_result()
-        .is_ok()
-        {
-            ptr as *mut u8
-        } else {
-            ptr::null_mut()
+        .branch();
+
+        match res {
+            ControlFlow::Continue(ptr) => ptr as *mut u8,
+            ControlFlow::Break(_) => ptr::null_mut(),
         }
     }
 

uefi_alloc/Cargo.toml
@@ -11,4 +11,4 @@ repository = "https://gitlab.redox-os.org/redox-os/uefi_alloc"
 name = "uefi_alloc"
 
 [dependencies]
-redox_uefi = "0.1.0"
+redox_uefi = "0.1.2"

commit fddaa80a1d50394384231fbf81007d0350c5d9ef

Fix global allocator
Don't overwrite the pointer with the status, which will be 0.

Fixes: 619d351 ("Update uefi dep, fix building on nightly")

Signed-off-by: Tim Crawford <tcrawford@system76.com>

uefi_alloc/src/lib.rs
@@ -29,7 +29,7 @@ unsafe impl GlobalAlloc for Allocator {
         .branch();
 
         match res {
-            ControlFlow::Continue(ptr) => ptr as *mut u8,
+            ControlFlow::Continue(_) => ptr as *mut u8,
             ControlFlow::Break(_) => ptr::null_mut(),
         }
     }