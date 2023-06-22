0200274b149e34398ae5ed00dda4ca49f01d9030
Tim Crawford <tcrawford@system76.com>
2023-06-22 08:31:34 -0600

    uefi: guid: Deprecate GuidKind
    Have users compare GUID values directly. This will remove the need to
    have all GUIDs defined within the GUID module itself, and can later be
    defined within the module for the protocol they define.
    
    Signed-off-by: Tim Crawford <tcrawford@system76.com>

--- crates/uefi/src/guid.rs
+++ crates/uefi/src/guid.rs
@@ -75,6 +75,7 @@ pub const COMPONENT_NAME2_GUID              : Guid = guid!("6a7a5cff-e8d9-4f70-b
 #[repr(C)]
 pub struct Guid(pub u32, pub u16, pub u16, pub [u8; 8]);
 
+#[deprecated(note = "use `Guid` directly")]
 #[derive(Copy, Clone, Debug, Eq, PartialEq)]
 pub enum GuidKind {
     Null,
@@ -170,6 +171,8 @@ impl Guid {
         Self(d1, d2, d3, d4)
     }
 
+    #[allow(deprecated)]
+    #[deprecated(note = "compare `Guid`s directly")]
     pub fn kind(&self) -> GuidKind {
         match *self {
             NULL_GUID => GuidKind::Null,
